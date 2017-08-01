b1d9ca8947b0e9dbf5181440daf0aff6c5f23cc9
aschoerk <aschoerk@gmx.net>
2017-08-01 11:19:54 +0200

    Handle ApplicationExceptions in Transactions, corrected usertransaction handling, corrected handling of not-ejb-classes

--- ejb-cdi-unit/src/main/java/com/oneandone/ejbcdiunit/internal/TransactionalInterceptor.java
+++ ejb-cdi-unit/src/main/java/com/oneandone/ejbcdiunit/internal/TransactionalInterceptor.java
@@ -1,6 +1,8 @@
 package com.oneandone.ejbcdiunit.internal;
 
+import javax.ejb.ApplicationException;
 import javax.ejb.EJB;
+import javax.ejb.EJBException;
 import javax.ejb.MessageDriven;
 import javax.ejb.Singleton;
 import javax.ejb.Stateful;
@@ -27,11 +29,26 @@ import com.oneandone.ejbcdiunit.persistence.SimulatedTransactionManager;
 public class TransactionalInterceptor {
 
 
-    private SimulatedTransactionManager transactionManager = new SimulatedTransactionManager();
-
     private final Logger logger =
             LoggerFactory.getLogger(TransactionalInterceptor.class);
+    private SimulatedTransactionManager transactionManager = new SimulatedTransactionManager();
 
+    private ApplicationException findApplicationException(Throwable ex) {
+        // search for applicationexception
+        Class<?> tmp = ex.getClass();
+        ApplicationException applicationException = null;
+        while (!tmp.equals(Throwable.class)) {
+            applicationException = tmp.getAnnotation(ApplicationException.class);
+            if (applicationException != null) {
+                break;
+            }
+            tmp = tmp.getSuperclass();
+        }
+        if (applicationException != null && (tmp.equals(ex.getClass()) || applicationException.inherited())) {
+            return applicationException;
+        }
+        return null;
+    }
 
     /**
      * called when a EjbTransactional annotated Bean is called.
@@ -46,12 +63,9 @@ public class TransactionalInterceptor {
         if (transactionManager.hasActiveTransactionInterceptor()) {
 
             final Class<?> declaringClass = ctx.getMethod().getDeclaringClass();
-            boolean beanManaged = (declaringClass.getAnnotation(TransactionManagement.class) != null
-                    && declaringClass.getAnnotation(TransactionManagement.class).value() == TransactionManagementType.BEAN);
-            if (declaringClass.getAnnotation(MessageDriven.class) == null
-                    && declaringClass.getAnnotation(Stateless.class) == null
-                    && declaringClass.getAnnotation(Stateful.class) == null
-                    && declaringClass.getAnnotation(Singleton.class) == null
+            Class<?> targetClass = getTargetClass(ctx);
+            boolean beanManaged = isBeanManaged(declaringClass) || isBeanManaged(targetClass);
+            if (isNotEjbClass(declaringClass) && isNotEjbClass(targetClass)
                     && ctx.getMethod().getAnnotation(EJB.class) == null
                     ) {
                 return ctx.proceed();
@@ -75,10 +89,29 @@ public class TransactionalInterceptor {
                 boolean passThroughRollbackException = true;
                 try {
                     return ctx.proceed();
-                } catch (RuntimeException ex) {
-                    passThroughRollbackException = false;
-                    transactionManager.rollback(false);
-                    throw ex;
+                } catch (Throwable ex) {
+                    if (beanManaged) {
+                        if (ex instanceof RuntimeException) {
+                            throw new EJBException((RuntimeException) ex);
+                        } else {
+                            throw ex;
+                        }
+                    }
+                    ApplicationException applicationException = findApplicationException(ex);
+                    boolean doRollback =
+                            applicationException != null ? applicationException.rollback() : ex instanceof RuntimeException;
+
+                    if (doRollback) {
+                        passThroughRollbackException = false;
+                        transactionManager.rollback(false);
+                        transactionManager.setRollbackOnly(false);
+                    }
+
+                    if (applicationException == null && ex instanceof RuntimeException) {
+                        throw new EJBException((RuntimeException) ex);
+                    } else {
+                        throw ex;
+                    }
                 } finally {
                     try {
                         transactionManager.pop();
@@ -93,5 +126,33 @@ public class TransactionalInterceptor {
             return ctx.proceed();
         }
 
+    }
+
+    private Class<?> getTargetClass(InvocationContext ctx) {
+        final Object target = ctx.getTarget();
+        if (target == null)
+            return null;
+        Class<? extends Object> res = target.getClass();
+        if (res.getName().endsWith("WeldSubclass"))
+            return res.getSuperclass();
+        else
+            return res;
+
+    }
+
+    private boolean isBeanManaged(Class<?> declaringClass) {
+        return declaringClass != null
+                && declaringClass.getAnnotation(TransactionManagement.class) != null
+                && declaringClass.getAnnotation(TransactionManagement.class).value() == TransactionManagementType.BEAN;
+    }
+
+    private boolean isNotEjbClass(Class<?> declaringClass) {
+        return declaringClass != null
+                && declaringClass.getAnnotation(TransactionManagement.class) == null // allow transactionmanagement to
+                                                                                     // be defined for non ejb-classes
+                && declaringClass.getAnnotation(MessageDriven.class) == null
+                && declaringClass.getAnnotation(Stateless.class) == null
+                && declaringClass.getAnnotation(Stateful.class) == null
+                && declaringClass.getAnnotation(Singleton.class) == null;
     }
 }
