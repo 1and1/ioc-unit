a76ea0446b211f42fb47603adc1dc8e547353cf2
Andreas Schörk <andreas.schoerk@1und1.de>
2019-01-15 22:08:19 +0100

    Renamed CdiUnit2 to IocUnit

--- ioc-unit-tests5/src/test/java/com/oneandone/ejbcdiunit5/ejb/TestStringResourceInjection.java
+++ ioc-unit-tests5/src/test/java/com/oneandone/ejbcdiunit5/ejb/TestStringResourceInjection.java
@@ -2,6 +2,7 @@ package com.oneandone.ejbcdiunit5.ejb;
 
 import javax.ejb.EJB;
 import javax.enterprise.inject.Produces;
+import javax.inject.Singleton;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -9,6 +10,7 @@ import org.junit.jupiter.api.extension.ExtendWith;
 
 import com.oneandone.iocunit.analyzer.annotations.SutClasses;
 import com.oneandone.iocunit.IocJUnit5Extension;
+import com.oneandone.iocunit.analyzer.annotations.TestClasses;
 import com.oneandone.iocunit.ejb.ResourceQualifier;
 import com.oneandone.ejbcdiunit.ejbs.ResourceTestEjb;
 
@@ -16,34 +18,40 @@ import com.oneandone.ejbcdiunit.ejbs.ResourceTestEjb;
  * @author aschoerk
  */
 @ExtendWith(IocJUnit5Extension.class)
-@SutClasses(ResourceTestEjb.class)
+@TestClasses({ResourceTestEjb.class, TestStringResourceInjection.TestResources.class})
 public class TestStringResourceInjection {
     @EJB
     ResourceTestEjb resourceTestEjb;
 
-    @Produces
-    @ResourceQualifier(name = "ResourceTestEjbAppName", lookup = "java:app/AppName", mappedName = "RTEAppName")
-    String appNameProducer() {
-        return "TestStringResourceInjectionAppName";
+    @Singleton
+    static class TestResources {
+
+        @Produces
+        @ResourceQualifier(name = "ResourceTestEjbAppName", lookup = "java:app/AppName", mappedName = "RTEAppName")
+        String appNameProducer() {
+            return "TestStringResourceInjectionAppName";
+        }
+
+        @Produces
+        @ResourceQualifier(name = "ResourceTestEjbModuleName", lookup = "java:module/ModuleName", mappedName = "RTEModuleName")
+        String moduleNameProducer() {
+            return "TestStringResourceInjectionModuleName";
+        }
+
+        @Produces
+        @ResourceQualifier(lookup = "java:app/AppName")
+        String appName2Producer() {
+            return "TestStringResourceInjectionAppName2";
+        }
+
+        @Produces
+        @ResourceQualifier(lookup = "java:module/ModuleName")
+        String moduleName2Producer() {
+            return "TestStringResourceInjectionModuleName2";
+        }
+
     }
 
-    @Produces
-    @ResourceQualifier(name = "ResourceTestEjbModuleName", lookup = "java:module/ModuleName", mappedName = "RTEModuleName")
-    String moduleNameProducer() {
-        return "TestStringResourceInjectionModuleName";
-    }
-
-    @Produces
-    @ResourceQualifier(lookup = "java:app/AppName")
-    String appName2Producer() {
-        return "TestStringResourceInjectionAppName2";
-    }
-
-    @Produces
-    @ResourceQualifier(lookup = "java:module/ModuleName")
-    String moduleName2Producer() {
-        return "TestStringResourceInjectionModuleName2";
-    }
 
 
     @Test
