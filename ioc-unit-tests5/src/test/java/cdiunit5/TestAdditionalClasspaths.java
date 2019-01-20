3ec1678967f89b6f56cf7fb7e6d89f1e862ef4e4
Andreas Schörk <andreas.schoerk@1und1.de>
2019-01-20 20:56:04 +0100

    fix test for Phase4

--- ioc-unit-tests5/src/test/java/cdiunit5/TestAdditionalClasspaths.java
+++ ioc-unit-tests5/src/test/java/cdiunit5/TestAdditionalClasspaths.java
@@ -6,11 +6,14 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 
+import com.oneandone.iocunit.analyzer.annotations.ExcludedClasses;
 import com.oneandone.iocunit.analyzer.annotations.SutClasspaths;
 import com.oneandone.iocunit.IocJUnit5Extension;
 import com.oneandone.iocunitejb.cdiunit.ExternalInterface;
+import com.oneandone.iocunitejb.resources.Resources;
 
 @SutClasspaths(ExternalInterface.class)
+@ExcludedClasses(Resources.class)
 @ExtendWith(IocJUnit5Extension.class)
 public class TestAdditionalClasspaths {
 
