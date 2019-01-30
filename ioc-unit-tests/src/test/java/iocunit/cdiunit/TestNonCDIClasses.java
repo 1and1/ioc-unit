4ab981085e4fcc12ea414ef253fa6ac1d949aa2c
Andreas Schörk <andreas.schoerk@1und1.de>
2019-01-30 12:23:24 +0100

    fixed Tests

--- ioc-unit-tests/src/test/java/iocunit/cdiunit/TestNonCDIClasses.java
+++ ioc-unit-tests/src/test/java/iocunit/cdiunit/TestNonCDIClasses.java
@@ -8,6 +8,7 @@ import org.junit.runner.RunWith;
 import com.oneandone.cdi.weldstarter.StarterDeploymentException;
 import com.oneandone.cdi.weldstarter.WeldSetupClass;
 import com.oneandone.iocunit.IocUnitRunner;
+import com.oneandone.iocunit.analyzer.annotations.AnalyzerFlags;
 import com.oneandone.iocunit.analyzer.annotations.TestClasses;
 
 import ch.qos.logback.classic.filter.ThresholdFilter;
@@ -15,6 +16,8 @@ import ch.qos.logback.core.filter.Filter;
 
 @RunWith(IocUnitRunner.class)
 @TestClasses(ThresholdFilter.class)
+// prevent EvaluatorFilter from being found
+@AnalyzerFlags(allowGuessing = false)
 public class TestNonCDIClasses {
 
     @Inject
