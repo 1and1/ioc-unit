8cd140981174cd409aaa01045353820566976ae6
Andreas Schörk <andreas.schoerk@1und1.de>
2019-12-16 12:41:35 +0100

    fix circular dependency

--- ioc-unit-resteasy/src/test/java/com/oneandone/iocunit/resteasytester/ClientBuilderTest.java
+++ ioc-unit-resteasy/src/test/java/com/oneandone/iocunit/resteasytester/ClientBuilderTest.java
@@ -3,22 +3,17 @@ package com.oneandone.iocunit.resteasytester;
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
 
-import javax.enterprise.inject.Produces;
 import javax.inject.Inject;
-import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.client.WebTarget;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
-import org.jboss.resteasy.client.jaxrs.ResteasyClientBuilder;
-import org.jboss.resteasy.client.jaxrs.engines.ApacheHttpClient4Engine;
 import org.jboss.resteasy.core.Dispatcher;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import com.oneandone.iocunit.IocUnitRunner;
 import com.oneandone.iocunit.analyzer.annotations.SutClasses;
-import com.oneandone.iocunit.resteasy.restassured.TestHttpClient;
 import com.oneandone.iocunit.resteasytester.resources.ExampleResource;
 
 /**
@@ -29,21 +24,13 @@ import com.oneandone.iocunit.resteasytester.resources.ExampleResource;
 public class ClientBuilderTest {
 
     @Inject
-    ClientBuilder clientBuilder;
+    WebTarget wt;
 
     @Inject
     private Dispatcher dispatcher;
 
-    @Produces
-    public ClientBuilder createClientBuilder() {
-        return new ResteasyClientBuilder()
-                .httpEngine(new ApacheHttpClient4Engine(new TestHttpClient(dispatcher)));
-    }
-
-
     @Test
     public void test() {
-        WebTarget wt = clientBuilder.build().target("/");
         Response invoke = wt
                 .path("/restpath/method1")
                 .request(MediaType.APPLICATION_JSON)
