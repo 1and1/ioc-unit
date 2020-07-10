a091ee513444afae8d3a2b80ee4a5f5405250fc8
Andreas Schörk <andreas.schoerk@1und1.de>
2020-07-11 00:14:03 +0200

    prepare release 2.0.32,

--- modules/ioc-unit-warcheck-plugin/src/main/java/net/oneandone/iocunit/Main.java
+++ modules/ioc-unit-warcheck-plugin/src/main/java/net/oneandone/iocunit/Main.java
@@ -49,7 +49,7 @@ public class Main {
     };
 
     public static void main(String[] args) {
-        File f = new File("/home/aschoerk/projects/github/ioc-unit-main/testing/ioc-unit-test-war/target/ioc-unit-test-war-2.0.33-SNAPSHOT.war");
+        File f = new File("/home/aschoerk/projects/github/ioc-unit-main/testing/ioc-unit-test-war/target/ioc-unit-test-war-2.0.32-SNAPSHOT.war");
         try {
             WarInfo warInfo = new WarAnalyzer().analyzeWarFile(f);
             final Class<WeldStarter> weldStarterClass = WeldStarter.class;
