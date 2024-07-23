099523d5fb840573885527d3c5049c6b38456cd1
Fahad Aizaz <fahad.aizaz@hotmail.com>
2024-07-23 13:28:43 +0200

    Fix regression bug for JUnit5 nested tests introduced by commit: 5efc6333
    - The commit 5efc6333; fix for out-of-memory introduced
      a regression bug.
    - The bug effected existing JUnit5 tests containing
      nested tests (@Nested)

--- modules/ioc-unit/src/main/java/com/oneandone/iocunit/IocJUnit5Extension.java
+++ modules/ioc-unit/src/main/java/com/oneandone/iocunit/IocJUnit5Extension.java
@@ -1,12 +1,13 @@
 package com.oneandone.iocunit;
 
 import java.lang.reflect.Constructor;
-import java.lang.reflect.Method;
 import java.net.URL;
+import java.util.Objects;
 
 import javax.enterprise.context.ApplicationScoped;
 
 import org.junit.jupiter.api.extension.AfterAllCallback;
+import org.junit.jupiter.api.extension.BeforeAllCallback;
 import org.junit.jupiter.api.extension.BeforeEachCallback;
 import org.junit.jupiter.api.extension.ExtensionContext;
 import org.junit.jupiter.api.extension.TestExecutionExceptionHandler;
@@ -16,103 +17,64 @@ import org.junit.jupiter.api.extension.TestInstantiationException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class IocJUnit5Extension implements BeforeEachCallback,
-        AfterAllCallback, TestExecutionExceptionHandler, TestInstanceFactory {
+public class IocJUnit5Extension implements //
+        BeforeEachCallback, //
+        BeforeAllCallback, //
+        AfterAllCallback, //
+        TestExecutionExceptionHandler, //
+        TestInstanceFactory {
 
-    private static Logger logger = LoggerFactory.getLogger(IocJUnit5Extension.class);
+    private static final Logger LOGGER = LoggerFactory.getLogger(IocJUnit5Extension.class);
+
     // global system property
     private static final String ABSENT_CODE_PREFIX = "Absent Code attribute in method that is not native or abstract in class file ";
-    IocUnitAnalyzeAndStarter analyzeAndStarter = new IocUnitAnalyzeAndStarter();
 
+    private IocUnitAnalyzeAndStarter analyzeAndStarter;
     protected Throwable startupException;
 
-
-    public IocJUnit5Extension() {
-
+    @Override
+    public void beforeAll(ExtensionContext context) {
+        LOGGER.trace("---->before all execution {} {}\n", context.getDisplayName(), this);
+        if (Objects.isNull(analyzeAndStarter)) {
+            analyzeAndStarter = new IocUnitAnalyzeAndStarter();
+        }
     }
 
     @Override
-    public void afterAll(final ExtensionContext extensionContext) throws Exception {
-        logger.trace("---->after All execution {} {}\n", extensionContext.getDisplayName(), this);
-        analyzeAndStarter.shutdownWeldIfRunning(false);
-        analyzeAndStarter = null;
+    public void afterAll(final ExtensionContext context) throws Exception {
+        LOGGER.trace("---->after all execution {} {}\n", context.getDisplayName(), this);
+        if (Objects.nonNull(analyzeAndStarter)) {
+            analyzeAndStarter.shutdownWeldIfRunning(false);
+            analyzeAndStarter = null;
+        }
     }
 
-
-    private Object createTestInstance(Class<?> clazz) throws Exception {
-        startupException = null;
-        analyzeAndStarter.shutdownWeldIfRunning(false);
-        if(!analyzeAndStarter.isRunning()) {
-            logger.trace("----> starting up Weld.");
-
-            startupException = analyzeAndStarter.checkVersion();
-            System.setProperty("java.naming.factory.initial", "com.oneandone.iocunit.naming.CdiTesterContextFactory");
-
-            try {
-                if (startupException == null)
-                    analyzeAndStarter.analyzeAndStart(clazz, null);
-            } catch (ClassFormatError e) {
-                startupException = parseClassFormatError(e);
-            } catch (Throwable e) {
-                if(startupException == null) {
-                    startupException = e;
-                }
-            } // store info about explicit param injection, either from global settings or from annotation on the test class
-            if(startupException != null) {
-                try {
-                    Constructor<?> c = clazz.getDeclaredConstructor();
-                    c.setAccessible(true);
-                    return c.newInstance();
-                } catch (Exception e) {
-                    logger.error("Exception during Startup: ", startupException);
-                    throw e;
+    @Override
+    public void beforeEach(final ExtensionContext context) throws Exception {
+        if (startupException != null) {
+            if (context.getTestMethod().isPresent() && context.getTestMethod().get().isAnnotationPresent(ExpectedStartupException.class)) {
+                ExpectedStartupException ann = context.getTestMethod().get().getAnnotation(ExpectedStartupException.class);
+                if (ann.value().isAssignableFrom(startupException.getClass())) {
+                    analyzeAndStarter.shutdownWeldIfRunning(true);
+                    return;
                 }
             }
-            analyzeAndStarter.initContexts();
-            Object test = analyzeAndStarter.getCreationalContexts().create(clazz, ApplicationScoped.class);
-            logger.trace("---->Found testinstance {}\n", test);
-            return test;
-
-        }
-        return null;
-    }
-
-
-
-
-    private ClassFormatError parseClassFormatError(ClassFormatError e) {
-        if(e.getMessage().startsWith(ABSENT_CODE_PREFIX)) {
-            String offendingClass = e.getMessage().substring(ABSENT_CODE_PREFIX.length());
-            URL url = IocJUnit5Extension.class.getClassLoader().getResource(offendingClass + ".class");
-
-            return new ClassFormatError("'" + offendingClass.replace('/', '.')
-                                        + "' is an API only class. You need to remove '"
-                                        + url.toString().substring(9, url.toString().indexOf("!")) + "' from your classpath");
-        }
-        else {
-            return e;
+            if (startupException instanceof Exception) {
+                throw (Exception) startupException;
+            } else {
+                throw new RuntimeException(startupException);
+            }
         }
     }
 
     @Override
-    public void handleTestExecutionException(ExtensionContext extensionContext, Throwable throwable) throws Throwable {
-        if(startupException != null) {
-            logger.info("\"{}\" Ignored because of StartupException \"{}\"", throwable, startupException.getMessage());
-            return;
-        }
-        throw throwable;
-    }
-
-    @Override
-    public Object createTestInstance(final TestInstanceFactoryContext testInstanceFactoryContext, final ExtensionContext extensionContext)
+    public Object createTestInstance(final TestInstanceFactoryContext testInstanceFactoryContext, final ExtensionContext context)
             throws TestInstantiationException {
         try {
-            logger.trace("---->createTestInstance {} {}", testInstanceFactoryContext.getTestClass(), testInstanceFactoryContext.getOuterInstance());
-            if(!testInstanceFactoryContext.getOuterInstance().isPresent()) {
-                Object test = this.createTestInstance(testInstanceFactoryContext.getTestClass());
-                return test;
-            }
-            else {
+            LOGGER.trace("---->createTestInstance {} {}", testInstanceFactoryContext.getTestClass(), testInstanceFactoryContext.getOuterInstance());
+            if (!testInstanceFactoryContext.getOuterInstance().isPresent()) {
+                return this.createTestInstance(testInstanceFactoryContext.getTestClass());
+            } else {
                 final Object outerInstance = testInstanceFactoryContext.getOuterInstance().get();
                 Constructor<?> c = testInstanceFactoryContext.getTestClass().getDeclaredConstructor(outerInstance.getClass());
                 c.setAccessible(true);
@@ -124,23 +86,71 @@ public class IocJUnit5Extension implements BeforeEachCallback,
     }
 
     @Override
-    public void beforeEach(final ExtensionContext extensionContext) throws Exception {
-        if(startupException != null) {
-            if(extensionContext.getTestMethod().isPresent()
-               && extensionContext.getTestMethod().get().isAnnotationPresent(ExpectedStartupException.class)) {
-                ExpectedStartupException ann = extensionContext.getTestMethod().get().getAnnotation(ExpectedStartupException.class);
-                if(ann.value().isAssignableFrom(startupException.getClass())) {
-                    analyzeAndStarter.shutdownWeldIfRunning(true);
-                    return;
-                }
-            }
-            if(startupException instanceof Exception) {
-                throw (Exception) startupException;
-            }
-            else {
-                throw new RuntimeException(startupException);
-            }
+    public void handleTestExecutionException(ExtensionContext context, Throwable throwable) throws Throwable {
+        if (startupException != null) {
+            LOGGER.info("\"{}\" Ignored because of StartupException \"{}\"", throwable, startupException.getMessage());
+            return;
         }
+        throw throwable;
     }
 
+
+    private Object createTestInstance(Class<?> clazz) throws Exception {
+        startupException = null;
+        if (Objects.isNull(analyzeAndStarter)) {
+            analyzeAndStarter = new IocUnitAnalyzeAndStarter();
+
+        }
+
+        analyzeAndStarter.shutdownWeldIfRunning(false);
+        if (!analyzeAndStarter.isRunning()) {
+            LOGGER.trace("----> starting up Weld.");
+
+            startupException = analyzeAndStarter.checkVersion();
+            System.setProperty("java.naming.factory.initial", "com.oneandone.iocunit.naming.CdiTesterContextFactory");
+
+            try {
+                if (startupException == null) {
+                    analyzeAndStarter.analyzeAndStart(clazz, null);
+                }
+            } catch (ClassFormatError e) {
+                startupException = parseClassFormatError(e);
+            } catch (Throwable e) {
+                if (startupException == null) {
+                    startupException = e;
+                }
+            }
+
+            // store info about explicit param injection, either from global settings or from annotation on the test class
+            if (startupException != null) {
+                try {
+                    Constructor<?> c = clazz.getDeclaredConstructor();
+                    c.setAccessible(true);
+                    return c.newInstance();
+                } catch (Exception e) {
+                    LOGGER.error("Exception during Startup: ", startupException);
+                    throw e;
+                }
+            }
+            analyzeAndStarter.initContexts();
+            Object test = analyzeAndStarter.getCreationalContexts().create(clazz, ApplicationScoped.class);
+            LOGGER.trace("---->Found test instance {}\n", test);
+            return test;
+
+        }
+        return null;
+    }
+
+
+    private ClassFormatError parseClassFormatError(ClassFormatError e) {
+        if (e.getMessage().startsWith(ABSENT_CODE_PREFIX)) {
+            String offendingClass = e.getMessage().substring(ABSENT_CODE_PREFIX.length());
+            URL url = IocJUnit5Extension.class.getClassLoader().getResource(offendingClass + ".class");
+            String fqdn = url.toString().substring(9, url.toString().indexOf("!"));
+            String m = String.format("'%s' is an API only class. You need to remove '%s' from classpath", offendingClass.replace('/', '.'), fqdn);
+            return new ClassFormatError(m);
+        } else {
+            return e;
+        }
+    }
 }
